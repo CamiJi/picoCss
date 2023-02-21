c4550e62956a03c9a44bd90c4e031c0cd6b833da
CamiJi <aubertcam@gmail.com>
2023-02-21 07:15:18 +0100

    Front article

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,5 +1,34 @@
 
 <x-layout>
-    hello {{$id}}
+    <hgroup>
+    <h1>PHP est mort</h1>
+    <h2>Le décès est survenu en 2089</h2>
+    <small>Publié le 01/01/2021 par Camille Aubert</small>
+    </hgroup>
+    <img src="https://via.placeholder.com/1300x300/2c3d49">
+    <h3>Quand</h3>
+    <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias ab, quam ratione quidem cum harum possimus amet sit voluptas soluta placeat a commodi at doloremque maxime quisquam, porro recusandae cumque.
+    Molestias sint sit odio at nam non maxime aperiam doloribus voluptatum modi voluptate eius culpa aut autem provident atque recusandae fugit et, accusamus, quisquam doloremque delectus quaerat. Laudantium, totam praesentium.
+    Saepe, quos expedita! Aperiam fugiat quibusdam, obcaecati magnam reprehenderit, pariatur nulla totam quia enim quidem ut, nemo odio nihil architecto aut a ipsam cum repellendus at quas earum impedit aspernatur.
+    Accusamus voluptas atque nesciunt pariatur dignissimos consequatur minus numquam unde qui, ullam, corrupti dolorem velit? Quaerat iusto debitis delectus optio, quo veniam illum ullam rerum sequi deleniti impedit similique maiores.
+    Architecto soluta, temporibus, ipsa repudiandae praesentium ratione ex cupiditate excepturi eligendi quod fuga rerum aspernatur! Quam quisquam quibusdam deserunt ratione. Magnam laboriosam nulla quaerat! Quae tempore dignissimos nulla sapiente harum!
+    </p>
+    <h3>Comment</h3>
+    <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias ab, quam ratione quidem cum harum possimus amet sit voluptas soluta placeat a commodi at doloremque maxime quisquam, porro recusandae cumque.
+    Molestias sint sit odio at nam non maxime aperiam doloribus voluptatum modi voluptate eius culpa aut autem provident atque recusandae fugit et, accusamus, quisquam doloremque delectus quaerat. Laudantium, totam praesentium.
+    Saepe, quos expedita! Aperiam fugiat quibusdam, obcaecati magnam reprehenderit, pariatur nulla totam quia enim quidem ut, nemo odio nihil architecto aut a ipsam cum repellendus at quas earum impedit aspernatur.
+    Accusamus voluptas atque nesciunt pariatur dignissimos consequatur minus numquam unde qui, ullam, corrupti dolorem velit? Quaerat iusto debitis delectus optio, quo veniam illum ullam rerum sequi deleniti impedit similique maiores.
+    Architecto soluta, temporibus, ipsa repudiandae praesentium ratione ex cupiditate excepturi eligendi quod fuga rerum aspernatur! Quam quisquam quibusdam deserunt ratione. Magnam laboriosam nulla quaerat! Quae tempore dignissimos nulla sapiente harum!
+    </p>
+    <h3>Pourquoi</h3>
+    <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias ab, quam ratione quidem cum harum possimus amet sit voluptas soluta placeat a commodi at doloremque maxime quisquam, porro recusandae cumque.
+    Molestias sint sit odio at nam non maxime aperiam doloribus voluptatum modi voluptate eius culpa aut autem provident atque recusandae fugit et, accusamus, quisquam doloremque delectus quaerat. Laudantium, totam praesentium.
+    Saepe, quos expedita! Aperiam fugiat quibusdam, obcaecati magnam reprehenderit, pariatur nulla totam quia enim quidem ut, nemo odio nihil architecto aut a ipsam cum repellendus at quas earum impedit aspernatur.
+    Accusamus voluptas atque nesciunt pariatur dignissimos consequatur minus numquam unde qui, ullam, corrupti dolorem velit? Quaerat iusto debitis delectus optio, quo veniam illum ullam rerum sequi deleniti impedit similique maiores.
+    Architecto soluta, temporibus, ipsa repudiandae praesentium ratione ex cupiditate excepturi eligendi quod fuga rerum aspernatur! Quam quisquam quibusdam deserunt ratione. Magnam laboriosam nulla quaerat! Quae tempore dignissimos nulla sapiente harum!
+    </p>
 
 </x-layout>

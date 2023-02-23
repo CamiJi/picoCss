a737268b6e0bf092af055c4a9d87177de4c746bf
CamiJi <aubertcam@gmail.com>
2023-02-23 08:02:30 +0100

    neumorphism

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,15 +1,17 @@
 
 <x-layout>
 
+    <div id="article">
 
-    <hgroup>
-        <h1>{{$article->title}}</h1>
-        <h2></h2>
-        <small>Publié le {{$article->updated_at}} par Camille Aubert</small>
+        <hgroup>
+            <h1>{{$article->title}}</h1>
+            <h2></h2>
+            <small>Publié le {{$article->updated_at->format('d/m/Y')}} par Camille Aubert</small>
         </hgroup>
         <br />
         <img src="{{ $article->image }}"> 
         <p>{{ $article->content }}</p>
-
+        
+    </div>
 
 </x-layout>

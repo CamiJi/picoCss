bced5ee552ff1eb7bac38b562a9abd639e9f3d53
CamiJi <aubertcam@gmail.com>
2023-02-28 08:18:35 +0100

    Going to work

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -5,11 +5,11 @@
 
         <hgroup>
             <h1>{{$article->title}}</h1>
-            <h2></h2>
+            <h4>{{$article->subtitle}}</h4>
             <small>Publié le {{$article->updated_at->format('d/m/Y')}} par Camille Aubert</small>
         </hgroup>
         <br />
-        <img src="{{ $article->image }}"> 
+        {{-- <img src="{{ $article->image }}">  --}}
         <p>{{ $article->content }}</p>
         
     </div>

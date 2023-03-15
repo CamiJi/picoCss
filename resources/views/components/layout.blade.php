be931121abfa68ce4fc750424da52895d6bb7e60
CamiJi <aubertcam@gmail.com>
2023-03-15 20:38:03 +0100

    Add favicons

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,9 +3,25 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
     <title>{{ $article->title ?? 'Camille Aubert - PicoBlog' }}</title>
+    <meta name="description" content="{{ $article->description ?? 'Blog de Camille Aubert, développeur web à Paris' }}">
+    <meta name="author" content="Camille Aubert">
+    <meta name="keywords" content="Camille Aubert, développeur web, blog, Paris, Laravel, PicoCss">
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta name="google" content="nositelinkssearchbox">
+    <meta name="google" content="notranslate">
+
+
+
+    {{-- Ajout des favicons source : https://favicon.io/favicon-converter/ --}}
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+
   </head>
   <body>
     <main class="container">

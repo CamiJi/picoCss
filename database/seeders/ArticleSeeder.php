b05513e84d93e30fa24fe7013ab57c396ab9bcb9
CamiJi <aubertcam@gmail.com>
2023-03-22 05:52:57 +0100

    Logo header mobile

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -16,7 +16,7 @@ class ArticleSeeder extends Seeder
         Article::create([
             'title' => 'Premier article',
             'subtitle' => 'Cette article est seedé par la classe ArticleSeeder',
-            'content' => 'Article seedé par la classe ArticleSeeder. La commande pour le ssedé est : php artisan db:seed --class=ArticleSeeder. Pour notre application nous avons crée  un model, une migration, un sedder et une factory sur le thème de l\'article',
+            'content' => 'Article seedé par la classe ArticleSeeder. La commande pour le seedé est : php artisan db:seed --class=ArticleSeeder.',
             'image' => 'https://www.qwant.com/?client=brz-moz&t=images&q=seeder&size=large&o=1%3ABB64D9D4A38E1A398C60F50F646A130336402021',
             'published_at' => now(),
         ]);
